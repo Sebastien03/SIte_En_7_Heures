c03c69b374bec63a8031026bc0e48dfe6433d032
Sebastien Dutripon <seb032@live.fr>
2017-05-03 14:17:29 +0200

    page d'accueil

--- index.php
+++ index.php
@@ -4,13 +4,173 @@
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<title> Site en 7 heures</title>
-			<link href="css/bootstrap.css" rel="stylesheet"> 
-			<link rel="stylesheet" type="text/css" href="style.css">
+			<link href="style/css/bootstrap.css" rel="stylesheet"> 
+			<link rel="stylesheet" type="text/css" href="style/css/style.css">
 		</head>
 		<body>
 			<div class="container">
-				<center> <h1> Bienvenue sur le site de la Guilde Elegeia </h1> </center>
-				<img src="image/bannière.jpg" alt=" La bannière de notre guilde" />
+				<header>
+					<img src="image/bannière.jpg" alt=" La bannière de notre guilde" width="100%" />
+					<center> <h1> Bienvenue sur le site de la Guilde Elegeia </h1> </center>
+					<center> <p> Il n'y a pas de mort, il n'y a que ... le centre médical </p> </center>
+					<nav class="navbar-header">
+						<span class="text navbar-toggle"> Navigation </span>
+						<span class="bars navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<i class="fa fa-bars"></i>
+						</span>
+					</nav>
+					<nav>
+						<ul class="nav navbar-nav">
+							<li class="dropdown">
+								<a href="index.php" title="Accueil" class="active"> Accueil </a>
+							</li>
+							<li class="dropdown">
+								<a href="#" title="Presentation" class="active"> Presentation </a>
+							</li>
+							<li class="dropdown">
+								<a href="#" title="Galerie" class="active"> Galerie </a>
+							</li>
+							<li class="dropdown">
+								<a href="#" title="Partenariat" class="active"> Nos partenaires </a>
+							</li>
+							<li class="dropdown">
+								<a href="#" title="Contact" class="active"> Contacte </a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				<div class="row">
+					<div class="col-md-9" id="contenus_accueil">
+						<p> Advenit post multos Scudilo Scutariorum tribunus velamento subagrestis ingenii persuasionis opifex callidus. qui eum adulabili sermone seriis admixto solus omnium proficisci pellexit vultu adsimulato saepius replicando quod flagrantibus votis eum videre frater cuperet patruelis, siquid per inprudentiam gestum est remissurus ut mitis et clemens, participemque eum suae maiestatis adscisceret, futurum laborum quoque socium, quos Arctoae provinciae diu fessae poscebant.
+
+						Hac ex causa conlaticia stipe Valerius humatur ille Publicola et subsidiis amicorum mariti inops cum liberis uxor alitur Reguli et dotatur ex aerario filia Scipionis, cum nobilitas florem adultae virginis diuturnum absentia pauperis erubesceret patris.
+
+						Novitates autem si spem adferunt, ut tamquam in herbis non fallacibus fructus appareat, non sunt illae quidem repudiandae, vetustas tamen suo loco conservanda; maxima est enim vis vetustatis et consuetudinis. Quin in ipso equo, cuius modo feci mentionem, si nulla res impediat, nemo est, quin eo, quo consuevit, libentius utatur quam intractato et novo. Nec vero in hoc quod est animal, sed in iis etiam quae sunt inanima, consuetudo valet, cum locis ipsis delectemur, montuosis etiam et silvestribus, in quibus diutius commorati sumus.
+						</p>
+					</div>
+					<div class="col-md-3" id="recrutement">
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th colspan="5"> <center> Recrutement </center> </th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td> Classe </td>
+									<td> Tank</td>
+									<td> Heal </td>
+									<td> Dps Distant </td>
+									<td> Dps Cac </td>
+								</tr>
+								<tr>
+									<td> Gardien Jedi </td>
+									<td class="oui"> Oui </td>
+									<td> </td>
+									<td> </td>
+									<td class="non"> Non </td>
+								</tr>
+								<tr>
+									<td> Ombre Jedi </td>
+									<td class="oui"> Oui </td>
+									<td> </td>
+									<td class="non"> Non </td>
+									<td class="non"> Non </td>
+								</tr>
+								<tr>
+									<td> Avant Garde </td>
+									<td class="oui"> Oui </td>
+									<td> </td>
+									<td class="non"> Non </td>
+									<td class="non"> Non </td>
+								</tr>
+								<tr>
+									<td> Erudit Jedi </td>
+									<td> </td>
+									<td class="oui"> Oui </td>
+									<td class="oui"> Oui </td>
+									<td class="oui"> Oui </td>
+								</tr>
+								<tr>
+									<td> Commando </td>
+									<td> </td>
+									<td class="oui"> Oui </td>
+									<td class="oui"> Oui </td>
+									<td> </td>
+								</tr>
+								<tr>
+									<td> Malfrat </td>
+									<td> </td>
+									<td class="oui"> Oui </td>
+									<td class="non"> Non </td>
+									<td class="non"> Non </td>
+								</tr>
+								<tr>
+									<td> Franc Tireur </td>
+									<td> </td>
+									<td> </td>
+									<td class="oui"> Oui </td>
+									<td class="oui"> Oui </td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<img src="image/republics.png" width="100%" height="300px">
+					</div>
+					<div class="col-md-6">
+						<img src="image/empire.png" width="100%" height="300px">
+					</div>
+				</div>
+				</br>
+				<div class="row">
+					<div class="col-md-3">
+						<img src="image/chevalier-jedi.jpg" width="100%" height="150px">
+					</div>
+					<div class="col-md-3">
+						<img src="image/jedi-consulaire.jpg" width="100%" height="150px">
+					</div>
+					<div class="col-md-3">
+						<img src="image/guerrier-sith.jpg" width="100%" height="150px">
+					</div>
+					<div class="col-md-3">
+						<img src="image/inquisiteur-sith.jpg" width="100%" height="150px">
+					</div>
+				</div>
+				</br>
+				<div class="row">
+					<div class="col-md-3">
+						<img src="image/soldat.jpg" width="100%" height="150px">
+					</div>
+					<div class="col-md-3">
+						<img src="image/contrebandier.jpg" width="100%" height="150px">
+					</div>
+					<div class="col-md-3">
+						<img src="image/agent-imperiale.jpg" width="100%" height="150px">
+					</div>
+					<div class="col-md-3">
+						<img src="image/chasseur-de-prime.jpg" width="100%" height="150px">
+					</div>
+				</div>
+				</br>
+				</br>
+				<div class="row">
+					<div class="col-md-3">
+						<center> <a href="#"> <h6> Accueil  </h6> </a> </center>
+					</div>
+					<div class="col-md-3">
+						<center> <a href="#"> <h6> Presentation </h6> </h6> </center>
+					</div>
+					<div class="col-md-3">
+						<center> <a href="#"> <h6> Galerie </h6> </a> </center>
+					</div>
+					<div class="col-md-3">
+						<center> <a href="#"> <h6> Partenariat </h6> </a> </center>
+					</div>
+				</div>
+				<center> <a href="#"> <h6 id="mention"> Mentions Légales </h6> </a> </center>
 			</div>
 		</body>
 	</html>
