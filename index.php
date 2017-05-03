0ff735b4bfd0d12e966965a0a5dbfae93b3a8aee
Sebastien Dutripon <seb032@live.fr>
2017-05-03 10:37:29 +0200

    Changement de dossier

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title> Site en 7 heures</title>
+			<link href="css/bootstrap.css" rel="stylesheet"> 
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</head>
+		<body>
+			<div class="container">
+				<center> <h1> Bienvenue sur le site de la Guilde Elegeia </h1> </center>
+				<img src="image/bannière.jpg" alt=" La bannière de notre guilde" />
+			</div>
+		</body>
+	</html>
